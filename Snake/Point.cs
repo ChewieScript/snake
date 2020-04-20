61f83849b894780bcf0fa4b6e9ba44ee118cd022
Nikita <korenkov0101@mail.ru>
2020-04-20 22:06:58 +0300

    add class Point

---
+++ Snake/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sum;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sum); 
+        }
+    }
+}
